e426e74bdd156bc5b42d6046043c70d084ead1b8
EG-easy <1018eguchi@gmail.com>
2020-10-21 19:51:05 +0900

    add component

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,7 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+Vue.config.devtools = true;
 Vue.config.productionTip = false;
+
+// import all componets here
+const ComponentContext = require.context("./", true, /\.vue$/i, "lazy");
+ComponentContext.keys().forEach((componentFilePath) => {
+  const componentName = componentFilePath.split("/").pop().split(".")[0];
+  Vue.component(componentName, () => ComponentContext(componentFilePath));
+});
 
 new Vue({
   router,
