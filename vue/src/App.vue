7aa5c2b490a7f82c21b3a03d082e45c0b50e96b3
YukiKAJIHARA <yuukikajihara.wcd@gmail.com>
2020-10-24 18:31:29 +0900

    INIT: re-setup vue project

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,21 +1,32 @@
 <template>
-  <div>
-    <router-view />
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<style>
-.sp-container {
-  margin: 0 auto;
-  max-width: 800px;
-  padding: 1rem;
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
-
-<script>
-export default {
-  created() {
-    this.$store.dispatch("init");
-  },
-};
-</script>
--- vue/src/main.js
+++ vue/src/main.js
@@ -1,23 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-import _ from "lodash";
-Object.defineProperty(Vue.prototype, "$lodash", { value: _ });
-
-Vue.config.devtools = true;
-Vue.config.productionTip = false;
-
-// import all componets here
-const ComponentContext = require.context("./", true, /\.vue$/i, "lazy");
-ComponentContext.keys().forEach((componentFilePath) => {
-  const componentName = componentFilePath.split("/").pop().split(".")[0];
-  Vue.component(componentName, () => ComponentContext(componentFilePath));
-});
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App)
+}).$mount('#app')
