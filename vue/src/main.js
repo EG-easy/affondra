dac2c47dd110c1ccab90e2813f906694d1c2e8f9
YukiKAJIHARA <yuukikajihara.wcd@gmail.com>
2020-10-24 18:49:39 +0900

    INIT: marge into last project

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,16 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import _ from "lodash";
+Object.defineProperty(Vue.prototype, "$lodash", { value: _ });
+
+Vue.config.devtools = true;
+Vue.config.productionTip = false;
+
+// import all componets here
+const ComponentContext = require.context("./", true, /\.vue$/i, "lazy");
+ComponentContext.keys().forEach((componentFilePath) => {
+  const componentName = componentFilePath.split("/").pop().split(".")[0];
+  Vue.component(componentName, () => ComponentContext(componentFilePath));
+});
 
 new Vue({
   router,
   store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,29 +1,20 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Index from "../views/Index.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    component: Index,
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
