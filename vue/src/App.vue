47418ff75ba9d8eae5447857132bf4882af497e0
YukiKAJIHARA <yuukikajihara.wcd@gmail.com>
2020-10-27 22:47:52 +0900

    ADD: bulma: a css framework

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -4,6 +4,37 @@
 </div>
 </template>
 
+<style lang="scss">
+@charset "utf-8";
+
+// Import a Google Font
+@import url('https://fonts.googleapis.com/css?family=Nunito:400,700');
+
+// Reduce radius of corners
+$radius-small: 1px;
+$radius: 2px;
+$radius-large: 3px;
+
+// Set your brand colors
+$purple: #8A4D76;
+$pink: #FA7C91;
+$brown: #757763;
+$beige-light: #D0D1CD;
+$beige-lighter: #EFF0EB;
+
+// Update Bulma's global variables
+$family-sans-serif: "Nunito",
+sans-serif;
+$grey-dark: $brown;
+$grey-light: $beige-light;
+$primary: $purple;
+$link: $pink;
+$widescreen-enabled: false;
+$fullhd-enabled: false;
+
+@import "../node_modules/bulma/bulma.sass";
+</style>
+
 <style>
 .sp-container {
   margin: 0 auto;
