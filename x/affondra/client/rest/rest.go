24ca65ad840830dd06b7b266a1bdd2e11eee1edc
EG-easy <1018eguchi@gmail.com>
2020-10-27 20:40:42 +0900

    add query by id

--- x/affondra/client/rest/rest.go
+++ x/affondra/client/rest/rest.go
@@ -13,6 +13,7 @@ func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
 	r.HandleFunc("/affondra/item", listItemHandler(cliCtx, "affondra")).Methods("GET")
 	r.HandleFunc("/affondra/item/owner/{ownerAddr}", getItemByOwner(cliCtx, "affondra")).Methods("GET")
 	r.HandleFunc("/affondra/item/denom/{denom}", getItemByDenom(cliCtx, "affondra")).Methods("GET")
+	r.HandleFunc("/affondra/item/{id}", getItemById(cliCtx, "affondra")).Methods("GET")
 	r.HandleFunc("/affondra/item", setItemHandler(cliCtx)).Methods("PUT")
 	r.HandleFunc("/affondra/item", deleteItemHandler(cliCtx)).Methods("DELETE")
 }
