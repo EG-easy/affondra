a2872d0fc957809cea5ea70a3ef9287e81d9f1d5
EG-easy <1018eguchi@gmail.com>
2020-10-29 08:20:12 +0900

    rest api done close #6

--- x/affondra/keeper/owners.go
+++ x/affondra/keeper/owners.go
@@ -10,20 +10,15 @@ func (k Keeper) GetOwner(ctx sdk.Context, addr sdk.AccAddress) (owner types.Owne
 	key := types.GetOwnerKey(addr)
 	b := store.Get(key)
 	if b == nil {
-		return types.NewOwner(addr, []string{}), false
+		return types.NewOwner(addr, []types.Item{}), false
 	}
 	k.cdc.MustUnmarshalBinaryLengthPrefixed(b, &owner)
 	return owner, true
 }
 
-func (k Keeper) SetOwner(ctx sdk.Context, addr sdk.AccAddress, ids []string) {
+func (k Keeper) SetOwner(ctx sdk.Context, addr sdk.AccAddress, owner types.Owner) {
 	store := ctx.KVStore(k.storeKey)
 	key := types.GetOwnerKey(addr)
-
-	var owner types.Owner
-	owner.Address = addr
-	owner.IDs = ids
-
 	value := k.cdc.MustMarshalBinaryLengthPrefixed(owner)
 	store.Set(key, value)
 }
