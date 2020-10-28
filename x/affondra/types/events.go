81509894a2c8c0e8adba4bd883a26e6818b6bc7b
EG-easy <1018eguchi@gmail.com>
2020-10-29 06:58:59 +0900

    add event msg

--- x/affondra/types/events.go
+++ x/affondra/types/events.go
@@ -11,8 +11,10 @@ const (
 
 	// TODO: Create keys fo your events, the values will be derivided from the msg
 	// AttributeKeyAddress  		= "address"
-	AttributeKeyDenom = "denom"
-	AttributeKeyNftId = "nft_id"
+	AttributeKeyDenom    = "denom"
+	AttributeKeyNftId    = "nft_id"
+	AttributeKeyReceiver = "receiver"
+	AttributeKeyID       = "ID"
 
 	// TODO: Some events may not have values for that reason you want to emit that something happened.
 	// AttributeValueDoubleSign = "double_sign"
