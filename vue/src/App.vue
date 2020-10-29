5714c27966ffec81fcb435d743d02a02da359bf7
YukiKAJIHARA <yuukikajihara.wcd@gmail.com>
2020-10-30 06:41:04 +0900

    MOD: ADD: menubar Login/Logout operation

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -27,10 +27,10 @@
             <a class="navbar-item">
               History
             </a>
-            <div class="navbar-item">
+            <div v-if="isLoggedIn" class="navbar-item">
               <ShowWalletBalance />
             </div>
-            <div class="navbar-item">
+            <div v-if="isLoggedIn" class="navbar-item">
               <button class="button is-rounded">
                 <span class="icon">
                   <svg style="width:24px;height:24px" viewBox="0 0 24 24">
@@ -40,7 +40,7 @@
                 <span>Get Tokens</span>
               </button>
             </div>
-            <div class="navbar-item">
+            <div v-if="!isLoggedIn" class="navbar-item">
               <button class="button is-rounded" @click="isShowLoginModal = true">
                 <span class="icon">
                   <svg style="width:24px;height:24px" viewBox="0 0 24 24">
@@ -48,6 +48,16 @@
                   </svg>
                 </span>
                 <span>Login</span>
+              </button>
+            </div>
+            <div v-if="isLoggedIn" class="navbar-item">
+              <button class="button is-rounded" @click="$store.dispatch('logout')">
+                <span class="icon">
+                  <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M10,17V14H3V10H10V7L15,12L10,17M10,2H19A2,2 0 0,1 21,4V20A2,2 0 0,1 19,22H10A2,2 0 0,1 8,20V18H10V20H19V4H10V6H8V4A2,2 0 0,1 10,2Z" />
+                  </svg>
+                </span>
+                <span>Logout</span>
               </button>
             </div>
           </div>
@@ -150,6 +160,11 @@ export default {
     return {
       isShowLoginModal: false,
       isShowListingModal: true,
+    }
+  },
+  computed: {
+    isLoggedIn() {
+      return this.$store.getters.isLoggedIn
     }
   },
   methods: {
