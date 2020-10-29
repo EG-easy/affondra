717839d6977a85e8b8f82187b581fbdcb4fcc79c
YukiKAJIHARA <yuukikajihara.wcd@gmail.com>
2020-10-30 05:53:17 +0900

    ADD: firebase npm package

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,3 +1,5 @@
+// const path = require('path');
+
 module.exports = {
   devServer: {
     disableHostCheck: true,
@@ -5,5 +7,20 @@ module.exports = {
   },
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  // configureWebpack: {
+  //   module:{
+  //     rules:[
+  //       {
+  //         test: /npm\.js$/,
+  //         loader: 'string-replace-loader',
+  //         include: path.resolve('node_modules/firebaseui/dist'),
+  //         query: {
+  //           search: 'require(\'firebase/app\');',
+  //           replace: 'require(\'firebase/app\').default;',
+  //         },
+  //       },
+  //     ]
+  //   }
+  // }
 }
